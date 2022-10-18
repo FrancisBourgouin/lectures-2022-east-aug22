3772d262d14c1a6166066e42b2d7f6a5d1f4ae98
Francis Bourgouin <netbeuix@gmail.com>
2022-10-18 19:25:01 -0400

    add w9d2-breakout lecture

---
+++ w9d2-breakout/blog/db/migrate/20221018213205_add_foreign_key_from_authors_to_posts.rb
@@ -0,0 +1,6 @@
+class AddForeignKeyFromAuthorsToPosts < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :author_id, :integer
+    add_foreign_key :posts, :authors
+  end
+end
---
+++ w9d2-breakout/rails-backend/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Author.create([
+  {name:"Bob", email:"bob@bob.com", description: "Hi Bob."},
+  {name:"Pequeno Pollo de la Pampa", email:"pock@pock.com", description: "Pock pock"},
+])
